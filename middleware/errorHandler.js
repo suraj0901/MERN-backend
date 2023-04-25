079bf80317b584b9c6fe18e24cec198055065007
suraj9j <suraj.jha@yolohealth.in>
2023-04-25 17:08:08 +0530

    ready to deploy

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -8,5 +8,5 @@ export const errorHandler = (err, req, res, next) => {
   console.log(err.stack);
   const status = res.statusCode ? res.statusCode : 500;
   res.status(status);
-  res.json({ message: err.message });
+  res.json({ message: err.message, isError: true });
 };
